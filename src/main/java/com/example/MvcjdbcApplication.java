a1caed1a36b297140951da88ff8751462fc51346
Maira <mllunaflores@gmail.com>
2017-01-12 16:04:32 -0600

    add schedule

--- src/main/java/com/example/MvcjdbcApplication.java
+++ src/main/java/com/example/MvcjdbcApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MvcjdbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcjdbcApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MvcjdbcApplication.class);
 	}
 }
