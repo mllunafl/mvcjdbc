7a38ab852834cd1787787fae951b901130e76298
Maira <mllunaflores@gmail.com>
2017-01-19 13:54:06 -0600

    bug when running

---
+++ src/main/java/com/example/controller/NutritionController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.domain.Nutrition;
+import com.example.service.NutritionService;
+
+public class NutritionController {
+
+	@Autowired
+	NutritionService nutritionService;
+	
+	 @GetMapping("/nutrition")
+	    public String nutritionForm(Model model) {
+	        model.addAttribute("nutrition", new Nutrition());
+	        return "nutrition";
+	    }
+
+	 @GetMapping("/nutritions")
+	 	public String nutritionAll(Model model){
+		 model.addAttribute("nutritions", nutritionService.findAll());
+		 return "nutritions";
+	 }
+	 
+	    @PostMapping("/nutrition")
+	    public String nutritionSubmit(Nutrition nutrition, Model model) {
+	    	nutritionService.add(nutrition);
+			 model.addAttribute("nutritions", nutritionService.findAll());
+	    	return "nutritions";
+	    }
+}
