99a949857b1af234910958e4723ddf63f068fc9f
Maira <mllunaflores@gmail.com>
2017-01-23 21:10:02 -0600

    add delete crud

--- src/main/java/com/example/controller/NutritionController.java
+++ src/main/java/com/example/controller/NutritionController.java
@@ -1,11 +1,17 @@
 package com.example.controller;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 import com.example.domain.Nutrition;
 import com.example.service.NutritionService;
@@ -27,6 +33,14 @@ public class NutritionController {
 		model.addAttribute("nutritions", nutritionService.findAll());
 		return "nutritions";
 	}
+	@PostMapping("/nutritions")
+	public String deleteNutritions(@RequestParam(value="deleteIds", required=true) List<Long> ids,Model model, WebRequest webRequest) {
+		System.out.println(ids);
+		nutritionService.delete(ids);
+		model.addAttribute("nutritions", nutritionService.findAll());
+		return "nutritions";
+	}
+	
 
 	@PostMapping("/nutrition")
 	public String nutritionSubmit(Nutrition nutrition, Model model) {
@@ -48,6 +62,26 @@ public class NutritionController {
 		return "nutr";
 	}
 	
-	@PostMapping("update")
-	public String nutritionUpdate()
+	@GetMapping("/edit-nutrition/{id}")
+	public String nutritionViewUpdate(@PathVariable("id") Integer id, Model model){
+		System.out.println("get edit nutrition" + id);
+		nutritionService.find(id);
+		model.addAttribute("nutrition",nutritionService.find(id));
+		return "update-nutr";
+	}
+	
+	@PostMapping("/edit-nutrition/{id}")
+	public String nutritionUpdate(Nutrition nutrition, Model model){
+		System.out.println("in nutrition" + nutrition);
+		nutritionService.update(nutrition);
+		model.addAttribute("nutrition", nutritionService.find((int) nutrition.getId()));
+		return "nutr";
+	}
+	
+	@RequestMapping("/delete-nutrition/{id}")
+	public String nutritonDelete(@PathVariable("id") Integer id, Model model){
+		nutritionService.delete(id);
+		model.addAttribute("nutritions", nutritionService.findAll());
+		return "/nutritions";
+	}
 }
